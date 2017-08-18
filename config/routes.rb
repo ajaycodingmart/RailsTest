2553d60e63e5a1b72ac06e5a95ebd9c6ee13026a
cmemp33 <ajay@codingmart.com>
2017-08-18 18:20:25 +0530

    Articles CRUD created

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :articles
   get 'articles/index'
-  root 'welcome#index'
+  get 'articles/show' => 'articles#show'
+  delete 'articles/destroy' => 'articles#destroy'
+  root 'articles#index'
 end
